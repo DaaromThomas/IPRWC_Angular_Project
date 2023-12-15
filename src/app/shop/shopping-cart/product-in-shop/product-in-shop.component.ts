6e18362c93008eb77963df10cf1d905bd02f7b8b
DaaromThomas <56thomasp56@gmail.com>
2023-12-15 17:53:59 +0100

    added pay button and changed shop-product container

--- src/app/shop/shopping-cart/product-in-shop/product-in-shop.component.ts
+++ src/app/shop/shopping-cart/product-in-shop/product-in-shop.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../interfaces/Product';
 import { CartService } from '../../../services/cart.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-product-in-shop',
   templateUrl: './product-in-shop.component.html',
-  styleUrl: './product-in-shop.component.less'
+  styleUrl: './product-in-shop.component.less',
 })
 export class ProductInShopComponent {
   @Input() product!: Product;
